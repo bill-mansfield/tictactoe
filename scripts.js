221dd48130da6afdc95451b80e1aee2a4c119d8d
bill <manbil03@gmail.com>
2020-02-18 22:19:22 +1030

    trashpile

--- scripts.js
+++ scripts.js
@@ -7,10 +7,10 @@ TancTonqTia
 Player Factory function
 */
 
-const Player = (name, isVsComputer) => {
+const Player = (name, isVsPlayer) => {
     const returnName = () => name;
-    const returnIsVsComputer = () => isVsComputer;
-    return { returnName, returnIsVsComputer }
+    const returnIsVsPlayer = () => isVsPlayer;
+    return { returnName, returnIsVsPlayer }
 };
 
 /*
@@ -29,6 +29,7 @@ const gameBoard = (() => {
     for (const element of board) {
         let square = document.createElement("div");
         square.setAttribute("id", element);
+        square.classList.add("game_square");
         square.addEventListener("click", assignTeam);
         gameWrapper.appendChild(square);
     }
@@ -36,35 +37,58 @@ const gameBoard = (() => {
     function assignTeam() {
 
     }
+    return { board, gameWrapper }
 
-})();
-
-/*
-Game Flow module 
-*/
-
-const gameFlow = (() => {
-    let nameInputWrapper = document.getElementById("name__wrapper");
-    let nameInputElement = document.getElementById("name_input");
-
-    const renderNameInput = (() => {
-        let gameTypeWrapper = document.getElementById("gametype__wrapper");
-        let vsComp = document.getElementById("vs_computer");
-
-        vsComp.addEventListener("click", revealNameInput);
-
-        function revealNameInput() {
-            nameInputWrapper.classList.add("show_element");
-            nameInputWrapper.classList.remove("hide_element");
-            gameTypeWrapper.classList.add("hide_element");
-        }
-    })();
-    let userOne = Player(nameInputElement.value, true);
 })();
 
 /*
 Display Controller Module
 */
 const displayController = (() => {
+    let gameTypeWrapper = document.getElementById("gametype__wrapper");
+    let nameInputWrapper = document.getElementById("name__wrapper");
+    let vsHuman = document.getElementById("vs_human");
+    let submitBtn = document.getElementById("name_submit");
+    const gameTitle = document.getElementById("game_title");
 
+    function revealNameInput() {
+        nameInputWrapper.classList.add("show_element");
+        nameInputWrapper.classList.remove("hide_element");
+        gameTypeWrapper.classList.add("hide_element");
+    }
+    const submitForm = () => {
+        const userNameOne = document.getElementById("name_input_one").value;
+        const userNameTwo = document.getElementById("name_input_two").value;
+
+        nameInputWrapper.classList.remove("show_element");
+        nameInputWrapper.classList.add("hide_element");
+
+        gameBoard.gameWrapper.classList.add("show_element");
+        gameBoard.gameWrapper.classList.remove("hide_element");
+        gameFlow.statePlayers();
+        return { userNameOne, userNameTwo }
+    }
+
+    submitBtn.addEventListener("click", submitForm)
+    vsHuman.addEventListener("click", revealNameInput);
+
+    return { submitForm, gameTitle }
+})();
+
+
+/*
+Game Flow module 
+*/
+
+const gameFlow = (() => {
+    const players = (() => {
+        playerOne = Player(displayController.submitForm.userNameOne, true);
+        playerTwo = Player(displayController.submitForm.userNameTwo, true);
+        return { playerOne, playerTwo}
+    })();
+
+    const statePlayers = (() => {
+        displayController.gameTitle.innerHTML = `${players.playerOne.returnName()} vs ${players.playerTwo.returnName()}`;
+    });
+    return { players, statePlayers }
 })();
